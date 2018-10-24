e8db3997ed9f30f1489af04e6903116b9e23ab41
RobertoMoralesDevs <roberto.morales.a@gmail.com>
2018-10-24 01:00:30 -0300

    Registar Usuario
    no puedo conectarme al php, mañana lo vuelvo a intentar

---
+++ Quisque/Assets/Scripts/API.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
+public class API : MonoBehaviour {
+
+	public InputField nameField;
+	public InputField passwordField;
+
+	public Button submitButton;
+
+
+	public void callRegister()
+	{
+		StartCoroutine(Register());
+	}
+
+	IEnumerator Register()
+	{
+		WWWForm form = new WWWForm();
+		form.AddField("name", nameField.text);
+		form.AddField("password", passwordField.text);
+
+		WWW www = new WWW("http://daniellaaraya.cl/Tomatica_de_soya/connection.php", form);
+		yield return www;
+		if(www.text == "0")
+		{
+			Debug.Log("User Created Succesfully");
+			UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+		}else
+		{
+			Debug.Log("User Creation failed" + www.text);
+		}
+
+	}
+	public void VerifyInputs() // para validaciones de campos
+	{
+		submitButton.interactable = (nameField.text.Length >= 8 && passwordField.text.Length >= 8);
+
+	}
+
+
+
+}
